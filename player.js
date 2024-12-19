430caae2e8197dc84e7a1d73942308eeca41309e
nmuddkidd <Nmuddkidd543@gmail.com>
2024-12-19 15:04:58 -0500

    ghoul and stuff

--- player.js
+++ player.js
@@ -2,19 +2,21 @@ class Player extends Phaser.GameObjects.Sprite{
     constructor(config){
         super(config.scene,config.x,config.y,'player');
         config.scene.add.existing(this);
-        //console.log("workin");
 
         this.scene = config.scene;
         this.addToUpdateList();
         this.setInteractive();
         this.scene.physics.world.enable(this);
-        //config.scene.setCollideWorldBounds(true);
+        //this.scene.setCollideWorldBounds(true);
 
         //0 - no direction
         //1 - down/right
         //-1 - up/left
-        this.Xdash = 0;
-        this.Ydash = 0;
+        this.Xmove = 0;
+        this.Ymove = 0;
+		
+		this.speed = 15;
+		
         this.keyObjects = this.scene.input.keyboard.addKeys({
 			W:Phaser.Input.Keyboard.KeyCodes.W,
 			A:Phaser.Input.Keyboard.KeyCodes.A,
@@ -22,36 +24,64 @@ class Player extends Phaser.GameObjects.Sprite{
 			D:Phaser.Input.Keyboard.KeyCodes.D,
 			SPACE:Phaser.Input.Keyboard.KeyCodes.SPACE
 		});
-    }
 
+        /*this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'turn',
+			frames: [ { key: 'dude', frame: 4 } ],
+			frameRate: 20
+		});
+
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+			frameRate: 10,
+			repeat: -1
+		});*/
+    }
+	
     preUpdate(time,delta){
         super.preUpdate(time,delta);
-
+		
         if (this.keyObjects.A.isDown){
-            this.body.setVelocityX(-15*delta);
-            this.Xdash = -1;
+            this.Xmove = -1;
         }else if (this.keyObjects.D.isDown){
-            this.body.setVelocityX(15*delta);
-            this.Xdash = 1;
+            this.Xmove = 1;
         }else{
-            this.body.setVelocityX(0);
-            this.Xdash = 0;
+            this.Xmove = 0;
         }
         
         if(this.keyObjects.S.isDown){
-            this.body.setVelocityY(15*delta);
-            this.Ydash = 1;
+            this.Ymove = 1;
         }else if(this.keyObjects.W.isDown){
-            this.body.setVelocityY(-15*delta);
-            this.Ydash = -1;
+            this.Ymove = -1;
         }else{
-            this.body.setVelocityY(0);
-            this.Ydash = 0;
+            this.Ymove = 0;
         }
+		
+		if(this.Xmove != 0 && this.Ymove != 0){
+			this.body.setVelocityX(this.Xmove*this.speed*.71*delta);
+			this.body.setVelocityY(this.Ymove*this.speed*.71*delta);
+		}else{
+			console.log(this.Xmove + " " + this.Ymove);
+			this.body.setVelocityX(this.Xmove*this.speed*delta);
+			this.body.setVelocityY(this.Ymove*this.speed*delta);
+		}
 
         if(Phaser.Input.Keyboard.JustDown(this.keyObjects.SPACE)){
-            this.body.setVelocityX(this.Xdash*400*delta);
-            this.body.setVelocityY(this.Ydash*400*delta);
+			if(this.Xmove != 0 && this.Ymove != 0){
+				this.body.setVelocityX(this.Xmove*this.speed*25*.71*delta);
+				this.body.setVelocityY(this.Ymove*this.speed*25*.71*delta);
+			}else{
+				this.body.setVelocityX(this.Xmove*this.speed*25*delta);
+				this.body.setVelocityY(this.Ymove*this.speed*25*delta);
+			}
         }
     }
 }
